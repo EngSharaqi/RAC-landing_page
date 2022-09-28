28b6e813e4af5c590a3efa1c4f2d10aa26f4c11e
EngSharaqi <muhammadsharaqi@gmail.com>
2022-09-29 00:09:11 +0200

    add all

--- public/js/main.js
+++ public/js/main.js
@@ -6,23 +6,32 @@ $('.center').slick({
         {
             breakpoint: 768,
             settings: {
-                arrows: false,
-                centerMode: true,
+                arrows: true,
+                centerMode: false,
                 centerPadding: '0',
-                slidesToShow: 3
+                slidesToShow: 1
             }
         },
         {
             breakpoint: 480,
             settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '0',
+                arrows: true,
+                centerMode: false,
+                centerPadding: '20px',
                 slidesToShow: 1
             }
         }
     ]
 });
+
+// customiza navigation
+let toggledSection = document.getElementsByClassName('navbar-toggler')[0];
+let mynav = document.getElementsByTagName('nav')[0];
+
+let custNavigation = () => {
+    // toggledSection.classList == 'collapsed' ? mynav.classList.add('collapsed') : mynav.classList.remove('collapsed')
+    mynav.classList.toggle('custNav');
+}
 
 // scroll to top 
 function scrollFunction() {
